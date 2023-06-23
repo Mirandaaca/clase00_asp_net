31833714defb789ffc2245924d55255f812fc142
Cristopher Aramayo <cristopher1012z@gmail.com>
2023-06-22 19:50:11 -1100

    Agregar archivos de proyecto.

---
+++ Controllers/PrincipalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace clase00_asp_net.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
